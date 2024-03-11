391ec3a6031c136f0ec46b245f5e2436517e821f
alexbulgakov <contact.bulgakov@gmail.com>
2024-03-12 02:15:34 +0700

    build: update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,17 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'perfectionist'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'plugin:perfectionist/recommended-line-length'
   ],
   root: true,
   env: {
     node: true,
-    jest: true
+    jest: true,
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -21,6 +20,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'perfectionist/sort-interfaces': 'error'
-  }
-};
+  },
+}
