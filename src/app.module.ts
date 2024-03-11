640e50c30c17514b0238f9b13152fa0da25306db
alexbulgakov <contact.bulgakov@gmail.com>
2024-03-12 05:53:28 +0700

    feat: add deleting deps for album, track, artists and favorites

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { DbModule } from './db/db.module'
+import { ArtistsModule } from './artists/artists.module'
+import { TracksModule } from './tracks/tracks.module'
+import { AlbumsModule } from './albums/albums.module'
+import { FavoritesModule } from './favorites/favorites.module'
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    DbModule,
+    ArtistsModule,
+    TracksModule,
+    AlbumsModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
